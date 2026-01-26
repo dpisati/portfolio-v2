c8bd997e9fb43d66399660883ac05245e8be210a
Daniel Pisati <dpisati@gmail.com>
2026-01-26 13:45:56 +1300

    Try fix build 02

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,8 @@
-import { Column, Heading, Text } from "@once-ui-system/core";
-
 export default function NotFound() {
   return (
-    <Column as="section" fill center paddingBottom="160">
-      <Text marginBottom="s" variant="display-strong-xl">
-        404
-      </Text>
-      <Heading marginBottom="l" variant="display-default-xs">
-        Page Not Found
-      </Heading>
-      <Text onBackground="neutral-weak">The page you are looking for does not exist.</Text>
-    </Column>
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
   );
 }
